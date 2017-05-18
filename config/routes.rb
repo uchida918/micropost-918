c7ac9688c64eeeb46231f866e4ffd5942f825a8f
Makoto Uchida <makoto_uchida918@yahoo.co.jp>
2017-05-18 09:00:59 +0000

    post

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
+  resources :microposts, only: [:create, :destroy]
 end
